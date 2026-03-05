5f25a6c04a93a77e5ec9b2db8a5a6039f99d3080
dhunsicker <122182482+dhunsicker@users.noreply.github.com>
2026-03-05 15:11:23 -0500

    Create HelloWorld_MrH.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld_MrH.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class HelloWorld_MrH extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "World");
+        telemetry.addData("Hello2", "World2");
+        telemetry.addData("Hello3", "World3");
+    }
+@Override
+    public void loop() {
+
+    }
+}
